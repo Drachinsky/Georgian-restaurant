2037ace06b3d5b065940413ef94beac2f67d2214
Drachinsky <fozy2772@gmail.com>
2022-06-07 23:50:21 +0300

    mod filed

--- main.js
+++ main.js
@@ -5,3 +5,32 @@ btnMenu.addEventListener('click', function() {
     btnMenu.classList.toggle('header__btn-menu--open');
     nav.classList.toggle('header__nav--open');
 });
+
+
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    direction: getDirection(),
+    spaceBetween: 5,
+    
+    loop: true,
+     
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+    },
+  
+    
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
+  
